77d845e79a23319c8edadea8503824b72ed29db8
alansaldivar87 <alansaldivar.87@gmail.com>
2023-06-11 21:22:27 -0700

    Add NewTweets component

---
+++ src/components/NewTweetForm.tsx
@@ -0,0 +1,78 @@
+import { useSession } from "next-auth/react";
+import Button from "./Button";
+import { ProfileImage } from "./ProfileImage";
+import {
+  type FormEvent,
+  useCallback,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
+import { api } from "~/utils/api";
+
+function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
+  if (!textArea) return;
+
+  textArea.style.height = "0";
+  textArea.style.height = `${textArea.scrollHeight}px`;
+}
+
+function Form() {
+  const session = useSession();
+  const [inputValue, setInputValue] = useState("");
+
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+
+  // Save the value of a ref in a callback
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
+
+  /**
+   * Required to calculate the height when the text area
+   */
+  useLayoutEffect(() => {
+    updateTextAreaSize(textAreaRef.current);
+  }, [inputValue]);
+
+  const createTweet = api.tweet.create.useMutation({
+    onSuccess: (newTweet) => {
+      console.log(newTweet);
+    },
+  });
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    createTweet.mutate({ text: inputValue });
+  };
+
+  if (session.status !== "authenticated") return null;
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 border-b px-4 py-2"
+    >
+      <div className="flex gap-4">
+        <ProfileImage src={session.data.user.image} />
+        <textarea
+          ref={inputRef}
+          style={{ height: 0 }}
+          onChange={(e) => setInputValue(e.target.value)}
+          className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
+          placeholder="What's happening?"
+        />
+      </div>
+
+      <Button className="self-end">Tweet</Button>
+    </form>
+  );
+}
+
+export function NewTweetForm() {
+  const session = useSession();
+  if (session.status !== "authenticated") return null;
+
+  return <Form />;
+}
